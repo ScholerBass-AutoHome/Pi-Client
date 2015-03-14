99e732e37c03106a42c5d91dc2319a39e81adb19
Andrew Bass <andybass009@gmail.com>
2015-03-14 18:04:59 -0400

    Add basis for Toggler

--- src/apps.rs
+++ src/apps.rs
@@ -1,10 +1,11 @@
 
+
 use std::collections::HashMap;
 
 pub type AppTable = HashMap<String, Appliance>;
 
 /// An appliance that is connected to the pi
-#[derive(Debug)]
+#[derive(Debug, Clone, RustcDecodable)]
 pub struct Appliance {
     pub on: bool,
     pub pin: i32,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate iron;
 extern crate router;
+extern crate "bodyparser" as bp;
+extern crate "rustc-serialize" as rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
@@ -14,26 +16,30 @@ mod apps;
 mod handler;
 
 fn handler(req: &mut Request) -> IronResult<Response> {
-    let query = req.extensions.get::<Router>().unwrap();
-    let found = query.find("app");
+    let json = req.get::<bp::Json>();
+    let query = req.extensions.find::<Router>().unwrap();
 
-    Ok(Response::with( (status::Ok, format!("{:?}", found)) ))
+    Ok(Response::with( (status::Ok, "HAA") ))
 }
 
 fn main() {
     let mut table = apps::AppTable::new();
 
     table.insert(
-        "lamp",
+        "lamp".to_string(),
         Appliance {
-
+           on: false,
+           pin: 0
         }
     );
 
     let mut router = Router::new();
 
     router.get("/apps/:app", handler);
-    router.get("/apps/:app/toggle", Toggler(
+    router.get("/apps/:app/toggle", handler);
+
+    router.get("/get-app", handler);
+    router.post("/set-app", handler);
 
     Iron::new(router).http("localhost:3000").unwrap();
 }
